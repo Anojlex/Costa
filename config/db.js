f8cf9f1609deb01d7ae8775b476e3ef8a4475019
Anojlex <112312778+Anojlex@users.noreply.github.com>
2023-08-24 19:08:19 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 function connect() {
-  mongoose.connect("mongodb+srv://costaAdmin:Admin@Costa@cluster0.pucj5a3.mongodb.net/?retryWrites=true&w=majority", {
+  mongoose.connect("mongodb+srv://costaAdmin:AdminCosta@cluster0.pucj5a3.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
